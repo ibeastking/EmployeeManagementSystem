d873ab80493578e4fc16540cef9c92338123c222
ibeastking <omajitpatil@gmail.com>
2025-02-28 17:52:02 +0530

    added docker file

--- backend/src/main/java/com/ems/backend/modal/Employee.java
+++ backend/src/main/java/com/ems/backend/modal/Employee.java
@@ -19,44 +19,4 @@ public class Employee {
     private String employeeEmail;
     private String employeeDepartment;
     private String employeePosition;
-
-    public int getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-
-    public String getEmployeeEmail() {
-        return employeeEmail;
-    }
-
-    public void setEmployeeEmail(String employeeEmail) {
-        this.employeeEmail = employeeEmail;
-    }
-
-    public String getEmployeeDepartment() {
-        return employeeDepartment;
-    }
-
-    public void setEmployeeDepartment(String employeeDepartment) {
-        this.employeeDepartment = employeeDepartment;
-    }
-
-    public String getEmployeePosition() {
-        return employeePosition;
-    }
-
-    public void setEmployeePosition(String employeePosition) {
-        this.employeePosition = employeePosition;
-    }
 }
